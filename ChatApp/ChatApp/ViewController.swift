e3e52abe95bb84379f0d21e349f2d543118916c4
Volodymyr Muraviov <volodymyr.muraviov@pm.bet>
2022-07-28 12:22:49 +0300

    initial commit

---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Volodymyr Muraviov on 28.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
